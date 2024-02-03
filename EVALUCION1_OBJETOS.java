8d77b4007ab53987bb770c93282d356d6bc8d7ab
Deltralmoon <156026478+Deltralmoon@users.noreply.github.com>
2024-02-02 18:00:44 -0700

    Add files via upload

---
+++ EVALUCION1_OBJETOS.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo1;
+
+/**
+ *
+ * @author ms016
+ */
+public class EVALUCION1_OBJETOS {
+    
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        persona persol;//declaro um identificador(variable de tipo persona)
+        //solo he declarado la variable , no eh creado el objet
+        //Crear el objeto(instancia) consiste en asignar
+    }
+    
+}
+
+//CLASE PERSONA
+/*
+//NombreDeLaClase --> Nomenclatura camecase
+class NombreDeLaClase [cuerpo dee la clase]
+*/
+class persona {
+    
+    
+}
